6da98241db6fe7676104633ab57e04b42503a936
pietrogiordanoc <36571527+pietrogiordanoc@users.noreply.github.com>
2025-08-13 14:59:33 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "https://focopilot-pietro-giordanos-projects.vercel.app"
+  origin: "https://fcpilot.vercel.app"
 }));
 
 app.use(express.static(path.join(__dirname, "../frontend")));
